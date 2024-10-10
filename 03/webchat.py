c83948af77466119201aab4d9b1cf9e157c8b737
Luri Darmawan <luri@carik.id>
2024-10-10 13:27:31 +0700

    web chat

---
+++ 03/webchat.py
@@ -0,0 +1,35 @@
+import os
+import gradio as gr
+from dotenv import load_dotenv
+load_dotenv()
+
+def respond(message, chat_history):
+    
+    answer = "test jawab dari: " + message
+    chat_history.append(("Bot", answer))
+
+    return chat_history, ""
+
+with gr.Blocks() as demo:
+    gr.Markdown("<h1>Web Chat BPS</h1>")
+
+    chatbot = gr.Chatbot()
+
+    with gr.Row():
+        with gr.Column():
+            message_box = gr.Textbox(
+                show_label=False, placeholder="Ketik pesan anda di sini", lines=1
+            )
+        with gr.Column():
+            send_button = gr.Button("Kirim")
+
+    send_button.click(respond, 
+        inputs=[message_box, chatbot],
+        outputs=[chatbot, message_box])
+    message_box.submit(respond, 
+        inputs=[message_box, chatbot],
+        outputs=[chatbot, message_box])
+
+PORT = os.getenv('APP_PORT', 8088)
+PORT = int(PORT)
+demo.launch(share=False, show_api=True, server_port=PORT)
